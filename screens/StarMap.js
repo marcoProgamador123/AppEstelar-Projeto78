045a1984e8f8ba5cdb6be81f99682a64eda1c4b1
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-05-28 19:26:01 -0300

    starMaps

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -19,7 +19,7 @@ export default class StarMapScreen extends Component {
                 <SafeAreaView style={styles.droidSafeArea} />
                 <View style={{ flex: 0.3, marginTop: 20, alignItems: 'center' }}>
                 <Text 
-                // style={styles.titleText}
+                 style={styles.titleText}
                 // style={{styles.titleText}}
                 // style=styles.titleText
                 // style={titleText}
@@ -48,11 +48,11 @@ export default class StarMapScreen extends Component {
                         //     })
                         // }}
 
-                        // onChangeText={(text) => {
-                        //     this.setState({
-                        //         longitude: text
-                        //     })
-                        // }}
+                         onChangeText={(text) => {
+                             this.setState({
+                                 longitude: text
+                             })
+                         }}
                     />
 
                     <TextInput
